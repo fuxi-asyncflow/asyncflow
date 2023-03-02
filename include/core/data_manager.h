f29a3e64f7a96cdd300e219c54cd7c5def82fd82
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2023-03-02 15:14:06 +0800

    add dtor for DataManager to avoid memory leak

--- include/core/data_manager.h
+++ include/core/data_manager.h
@@ -11,12 +11,24 @@ namespace asyncflow
 			using ChartDataMap = std::unordered_map<std::string, asyncflow::core::ChartData*>;
 
 		public:
-			ChartDataMap chart_data_;
+			DataManager() = default;
+			~DataManager()
+			{
+			    for (auto kv : chart_data_)
+			    {
+					auto* chart = kv.second;
+					delete chart;
+			    }
+				chart_data_.clear();
+			}
 
 			ChartDataMap* GetChartData()
 			{				
 				return &chart_data_;
 			}
+
+		private:
+			ChartDataMap chart_data_;
 		};
 	}
 }
