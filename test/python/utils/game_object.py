7515d3be09a43c5400ef19ce7dd08de981358b9a
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2023-03-03 19:09:31 +0800

    [py] fix error in python2

--- test/python/utils/game_object.py
+++ test/python/utils/game_object.py
@@ -1,10 +1,10 @@
 
 class Character:
-    def _output(msg):
+    def _output(self, msg):
         print(msg)
 
     def __init__(self, name):
         self.name = name
 
     def Say(self, msg):
-        Character._output("{name}: {msg}".format(name = self.name, msg = msg))
+        self._output("{name}: {msg}".format(name = self.name, msg = msg))
